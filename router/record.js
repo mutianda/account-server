c1d30e420128e0252d45f30447b80b0e785787b6
mutianda <qingyi.zongbu@qq.com>
2021-08-27 19:35:54 +0800

    sss

--- router/record.js
+++ router/record.js
@@ -2,8 +2,16 @@ var { conn ,app,Result}  = require('../index')
 const tree = {}
 app.post('/getRecord',  (req, res) => {
 	console.log('add');
-	let {startTime='' , endTime='',reason=''} = req.body
-	let sql = `select * from account_record where account_time > ${startTime} and account_time < ${endTime} and account_reason LIKE '%${reason}%'  order by  account_time desc `
+	let {startTime='' , endTime='',reason='',desc=''} = req.body
+	let sql = `select * from account_record where account_time > ${startTime} and account_time < ${endTime}   `
+	if(desc){
+		sql +=`and account_desc LIKE '%${desc}%' `
+	}
+	if(reason){
+		sql +=`and account_reason = '${reason}' `
+	}
+	sql+=` order by  account_time desc`
+	console.log(sql);
 	conn(sql).then(row=>{
 		if(row){
 			res.json(new Result({data:row,msg:'查询成功'}))
@@ -15,11 +23,11 @@ app.post('/getRecord',  (req, res) => {
 
 app.post('/addRecord',  (req, res) => {
 	console.log('add');
-	let {money ,time, reason='',desc=''} = req.body
+	let {money ,time, reason='',nemo=''} = req.body
 	if(!time){
 		res.json(new Result({code:0,msg:'参数不完整'}))
 	}
-	let sql = `INSERT INTO account_record (account_reason,account_time,account_money) VALUES ( '${reason}',  ${time} , ${money})`
+	let sql = `INSERT INTO account_record (account_reason,account_time,account_money,account_desc) VALUES ( '${reason}',  ${time} , ${money},'${nemo}')`
 	conn(sql).then(row=>{
 		if(row){
 			res.json(new Result({data:row,msg:'新增成功'}))
@@ -30,8 +38,8 @@ app.post('/addRecord',  (req, res) => {
 
 })
 app.post('/editRecord',  (req, res) => {
-	let {money ,time, reason='',desc='',id} = req.body
-	let sql = `UPDATE  account_record set account_reason = ${'\''+reason+'\''},account_money = ${'\''+money+'\''} where id = ${id}`
+	let {money ,time, reason='',nemo='',id} = req.body
+	let sql = `UPDATE  account_record set account_desc = ${'\''+nemo+'\''},account_reason = ${'\''+reason+'\''},account_money = ${'\''+money+'\''} where id = ${id}`
 	conn(sql).then(row=>{
 		if(row){
 			res.json(new Result({data:row,msg:'编辑成功'}))
